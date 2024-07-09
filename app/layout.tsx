5b9d1520f60b0a8899fd7501809aed80db2c6e14
Andreas Rayias <wowcaeu.andreas@gmail.com>
2024-07-10 02:09:52 +0300

    add auth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavMenu from "./NavMenu";
+import AuthProvider from "./AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,13 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} grid grid-rows-[auto,1fr] min-h-screen`}
-      >
-        <NavMenu />
-        <div className="mt-3 mx-auto w-4/6">{children}</div>
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body
+          className={`${inter.className} grid grid-rows-[auto,1fr] min-h-screen`}
+        >
+          <NavMenu />
+          <div className="mt-3 mx-auto w-4/6">{children}</div>
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
