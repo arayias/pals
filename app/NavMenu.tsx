bd0371f8dbadc888a02e23b59b8e4c17225df169
Andreas Rayias <wowcaeu.andreas@gmail.com>
2024-07-10 01:16:47 +0300

    add nav

---
+++ app/NavMenu.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function NavMenu() {
+  return (
+    <nav className="w-screen p-5 bg-gray-900 border-b border-gray-800">
+      <ul className="flex gap-2 justify-around">
+        <li className="hover:text-gray-300 transition-colors">
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li className="hover:text-gray-300 transition-colors">
+          <Link href={"/about"}>About</Link>
+        </li>
+        <li className="hover:text-gray-300 transition-colors">
+          <Link href={"/blog"}>Blog</Link>
+        </li>
+        <li className="hover:text-gray-300 transition-colors">
+          <Link href={"/users"}>Users</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
