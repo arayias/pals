4240bad81bedd6454d50f71c11cdaae53ea62bfe
Andreas Rayias <wowcaeu.andreas@gmail.com>
2024-07-10 18:36:40 +0300

    fixup skeleton and add PUT route to edit user

---
+++ app/api/user/route.ts
@@ -0,0 +1,28 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+
+export async function PUT(req: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user || !session.user.id) {
+    return NextResponse.redirect("/api/auth/signin");
+  }
+  const currentId = session.user?.id;
+
+  const data = await req.json();
+
+  const { name, bio } = data;
+  if (!bio || !name) {
+    return NextResponse.error();
+  }
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentId,
+    },
+    data: data,
+  });
+
+  return NextResponse.json(user);
+}
--- app/users/[id]/loading.tsx
+++ app/users/[id]/loading.tsx
@@ -3,15 +3,15 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default async function SkeletonLoadingUser() {
   return (
     <div className="flex flex-col gap-1">
-      <Skeleton className="w-[10%] h-4" />
-      <Skeleton className="w-[40%] h-[300px]" />
+      <Skeleton className="w-[10%] h-5 bg-slate-800" />
+      <Skeleton className="w-[40%] h-[400px] bg-slate-800" />
 
-      <Skeleton className="w-[7%] h-4" />
+      <Skeleton className="w-[7%] h-4 bg-slate-800" />
       <div className="flex flex-col space-y-2">
-        <Skeleton className="w-[40%] h-4" />
-        <Skeleton className="w-[10%] h-4" />
-        <Skeleton className="w-[90%] h-4" />
-        <Skeleton className="w-[80%] h-4" />
+        <Skeleton className="w-[40%] h-5 bg-slate-800" />
+        <Skeleton className="w-[10%] h-5 bg-slate-800" />
+        <Skeleton className="w-[90%] h-5 bg-slate-800" />
+        <Skeleton className="w-[80%] h-5 bg-slate-800" />
       </div>
     </div>
   );
