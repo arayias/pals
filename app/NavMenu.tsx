5c32fbf317b227dc6f0d665593dacc8080ab3c30
Andreas Rayias <wowcaeu.andreas@gmail.com>
2024-07-12 00:10:12 +0300

    Refactor NavMenu component to improve readability and responsiveness

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -4,22 +4,24 @@ import Link from "next/link";
 export default function NavMenu() {
   return (
     <nav className="w-full pt-3 pb-1 bg-gray-900 border-b border-gray-800 sticky top-0 z-10">
-      <ul className="flex gap-2 justify-around items-center w-4/6 mx-auto">
+      <ul className="flex justify-between items-center w-4/6 mx-auto">
         <li className="hover:text-gray-300 transition-colors">
           <Link href={"/"}>Home</Link>
         </li>
-        <li className="hover:text-gray-300 transition-colors">
-          <Link href={"/about"}>About</Link>
-        </li>
-        <li className="hover:text-gray-300 transition-colors">
-          <Link href={"/blog"}>Blog</Link>
-        </li>
-        <li className="hover:text-gray-300 transition-colors">
-          <Link href={"/users"}>Users</Link>
-        </li>
-        <li>
-          <UserMenu />
-        </li>
+        <div className="flex gap-3 ml-auto items-center">
+          <li className="hover:text-gray-300 transition-colors">
+            <Link href={"/about"}>About</Link>
+          </li>
+          <li className="hover:text-gray-300 transition-colors">
+            <Link href={"/blog"}>Blog</Link>
+          </li>
+          <li className="hover:text-gray-300 transition-colors">
+            <Link href={"/users"}>Users</Link>
+          </li>
+          <li>
+            <UserMenu />
+          </li>
+        </div>
       </ul>
     </nav>
   );
