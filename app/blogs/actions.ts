baed9ba9fa34e3d9aa087318d2b846d960a09d52
Andreas Rayias <wowcaeu.andreas@gmail.com>
2024-07-12 17:36:30 +0300

    extra check for negative pages in server actiojn

--- app/blogs/actions.ts
+++ app/blogs/actions.ts
@@ -3,6 +3,7 @@
 import { prisma } from "@/lib/prisma";
 
 export async function getBlogs(page: number, limit: number) {
+  if (page < 0) page = 0;
   return await prisma.blog.findMany({
     skip: page * limit,
     select: {
