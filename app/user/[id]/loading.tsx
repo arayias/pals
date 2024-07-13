42143211acd06b5def2689e0f0174193ce234859
Andreas Rayias <wowcaeu.andreas@gmail.com>
2024-07-13 23:56:37 +0300

    adjust loading.tsx for user/[id] to reflect changes made in previous commit

--- app/user/[id]/loading.tsx
+++ app/user/[id]/loading.tsx
@@ -1,8 +1,9 @@
+import { BlogCardSkeleton } from "@/app/blogs/loading";
 import { Skeleton } from "@/components/ui/skeleton";
 
-export default async function SkeletonLoadingUser() {
+function SkeletonLoadingUser() {
   return (
-    <div className="flex flex-col gap-1">
+    <div className="flex flex-col gap-1 md:border-r md:border-gray-200 md:pr-2 md:border-opacity-50">
       <Skeleton className="w-[10%] h-5 bg-slate-800" />
       <Skeleton className="w-[40%] aspect-square bg-slate-800" />
 
@@ -12,6 +13,22 @@ export default async function SkeletonLoadingUser() {
         <Skeleton className="w-[10%] h-5 bg-slate-800" />
         <Skeleton className="w-[90%] h-5 bg-slate-800" />
         <Skeleton className="w-[80%] h-5 bg-slate-800" />
+      </div>
+    </div>
+  );
+}
+
+export default async function SkeletonLoadingUserProfile() {
+  return (
+    <div className="grid grid-cols-1 gap-5 p-4 max-h-screen md:grid-cols-2">
+      <SkeletonLoadingUser />
+      <div className="max-h-screen flex flex-col">
+        <h2>Recent Blogs</h2>
+        <div className="overflow-y-auto grid grid-cols-1 gap-4 scroll-pr-5">
+          {Array.from({ length: 7 }).map((_, i) => (
+            <BlogCardSkeleton key={i} />
+          ))}
+        </div>
       </div>
     </div>
   );
