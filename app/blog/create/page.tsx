af029aead62d1eb5030c34c8c4d7446bdf2ff935
Andreas Rayias <wowcaeu.andreas@gmail.com>
2024-07-13 23:02:45 +0300

    provide a descriptive view when attempting to access blog/create and not logged in

--- app/blog/create/page.tsx
+++ app/blog/create/page.tsx
@@ -1,5 +1,18 @@
+import { getServerSession } from "next-auth";
 import CreateBlogForm from "./CreateBlogForm";
+import { authOptions } from "@/lib/auth";
 
-export default function BlogCreate() {
+export default async function BlogCreate() {
+  const session = await getServerSession(authOptions);
+  const isLoggedIn = session?.user?.id;
+  if (!isLoggedIn) {
+    return (
+      <>
+        <div className="border-l-4 p-4 border-orange-500">
+          <span>Please login to create a blog</span>
+        </div>
+      </>
+    );
+  }
   return <CreateBlogForm />;
 }
