9e176ec3d092e2107a6dfc332394836868a6e32d
Andreas Rayias <wowcaeu.andreas@gmail.com>
2024-07-13 23:50:48 +0300

    Add recent blogs section to user page

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import FollowButton from "@/components/FollowButton";
+import BlogCard from "@/components/BlogCard";
 
 type Props = {
   params: {
@@ -13,24 +14,52 @@ export default async function User({ params }: Props) {
       id: params.id,
     },
   });
+
+  const blogs = await prisma.blog.findMany({
+    where: {
+      authorId: params.id,
+    },
+    take: 7,
+  });
+
   const { name, bio, image } = user || {};
 
   return (
-    <div>
-      <h1>{name}</h1>
-      {image ? (
-        <div className="w-[50%]">
-          <img
-            className="w-full h-auto"
-            src={image}
-            alt={name ? `${name}'s Profile Image` : "Profile Image"}
-          />
-        </div>
-      ) : null}
+    <div className="grid grid-cols-1 gap-5 p-4 max-h-screen md:grid-cols-2">
+      <div className="md:border-r md:border-gray-200 md:pr-2 md:border-opacity-50">
+        <h1>{name}</h1>
+        {image ? (
+          <div className="w-[50%]">
+            <img
+              className="w-full h-auto"
+              src={image}
+              alt={name ? `${name}'s Profile Image` : "Profile Image"}
+            />
+          </div>
+        ) : null}
 
-      <h2>Bio</h2>
-      <p>{bio || "This user hasn't written a bio yet. Check back later!"}</p>
-      <FollowButton targetUserId={params.id} />
+        <h2>Bio</h2>
+        <p>{bio || "This user hasn't written a bio yet. Check back later!"}</p>
+        <FollowButton targetUserId={params.id} />
+      </div>
+      <div className="max-h-screen flex flex-col">
+        <h2>Recent Blogs</h2>
+        <div className="overflow-y-auto grid grid-cols-1 gap-4 scroll-pr-5">
+          {blogs.length > 0 ? (
+            blogs.map((blog) => (
+              <BlogCard
+                key={blog.id}
+                params={{
+                  ...blog,
+                  author: { name: user?.name!, image: user?.image ?? null },
+                }}
+              />
+            ))
+          ) : (
+            <p>This user hasn't written any blogs yet. Check back later!</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
